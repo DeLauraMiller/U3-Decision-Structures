9086f9c88479a9756cfefa273104667af8c406c9
millede000 <millede000@warren.k12.in.us>
2021-11-19 12:23:57 -0500

    quizgame

--- L3-Random Numbers/src/QuizGame.java
+++ L3-Random Numbers/src/QuizGame.java
@@ -1,2 +1,88 @@
+import javax.swing.*;
+import java.util.Random;
+
+
 public class QuizGame {
+    static int userAnswer, correctAnswer;
+    static String displayProblem = "";
+
+    public static void main(String[] args) {
+
+        int choice = input ("Choose addition(1), Subtraction(2), Multiplication(3), Division(4)");
+
+        createProblem(choice);
+
+        System.out.println(displayProblem);
+        System.out.println(correctAnswer);
+
+
+
+    public static void createProblem(int choice) {
+            if(choice >= 1){;
+                if(choice<5){
+                    if(choice ==1){
+                        add();
+                    }
+                    if (choice == 2){
+                        sub();
+                    }
+                    if(choice == 3){
+                        mult();
+                    }
+                    if(choice==4){
+                        div();
+                    }
+                }else {System.out.println("Wrong input, restart program, try again");
+                    System.exit(0);
+                }
+
+            }else{
+                System.out.println("Wrong input, restart program, try again");
+                System.exit(0);
+            }
+
+
+            System.out.println(displayProblem);
+            System.out.println(correctAnswer);
+        }
+    }
+    public static void add(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = num1 + num2;
+        displayProblem = num1 + " + " + num2;
+    }
+
+    public static void sub(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = num1 - num2;
+        displayProblem = num1 + " - " + num2;
+    }
+    public static void mult(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = num1 * num2;
+        displayProblem = num1 + " X " + num2;
+    }
+    public static void div(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = (num1 / num2);
+        displayProblem = num1 + " / " + num2;
+    }
+    //HELPER METHOD
+    public static int random() {
+        Random random = new Random();
+        return random.nextInt(20)+1;
+    }
+    public static int input(String message){
+        return Integer.parseInt(JOptionPane.showInputDialog(message));
+    }
+
 }
+
